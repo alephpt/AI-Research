cbbd9587c74ccf360149e14a24101439074e2621
alephpt <alephpt1@gmail.com>
2023-02-13 22:28:01 -0600

    well it doesn't quite work

--- Cpp/CARL/Alpha_Dev/CNN/CNN.cpp
+++ Cpp/CARL/Alpha_Dev/CNN/CNN.cpp
@@ -1,18 +1,12 @@
 #include "CNN.h"
 
-CNN::CNN() : n_inputs(0), n_input_layers(0) { appendNewLayer(createNewLayer(CNN_CONVOLUTION_LAYER)); }
-
-CNN::CNN(ftensor3d input, int n_inputs)
-{
-    appendNewLayer(createNewLayer(CNN_CONVOLUTION_LAYER));
-    n_input_layers = n_inputs;
-    inputs = input;
-    n_inputs = (int)inputs.size() / n_input_layers;
-}
+CNN::CNN() : n_inputs(0), n_input_layers(0) {}
 
 CNN::~CNN() {
     for (int l = 0; l < n_layers; l++) {
-        layers[l]->kernels.clear();
+        if (layers[l]->type == CNN_CONVOLUTION_LAYER) {
+            getConvolutionalData(layers[l])->kernels.clear();
+        }
     }
 
     layers.clear();
@@ -21,16 +15,19 @@ CNN::~CNN() {
 }
 
 void CNN::addNewLayer(CNNLayerType lt) { appendNewLayer(createNewLayer(lt)); }
+
 void CNN::addNewKernel(FilterStyle fs) { appendNewKernel(createNewKernel(fs)); };
 void CNN::addNewKernel(FixedFilterDimensions fd) { appendNewKernel(createNewKernel(fd)); };
 void CNN::addNewKernel(FixedFilterDimensions fd, FilterStyle fs) { appendNewKernel(createNewKernel(fd, fs)); };
 void CNN::addNewKernel(DynamicFilterDimensions fd, int n) { appendNewKernel(createNewKernel(fd, n)); };
 void CNN::addNewKernel(DynamicFilterDimensions fd, int n, FilterStyle fs) { appendNewKernel(createNewKernel(fd, n, fs)); };
 
-int CNN::getLayerCount() { return n_layers; }
 CNNLayer* CNN::getCurrentLayer() { return layers[n_layers - 1]; }
 CNNLayer* CNN::getPreviousLayer() { return layers[n_layers - 2]; }
-void CNN::appendNewKernel(Kernel* kernel) { getCurrentLayer()->kernels.push_back(kernel); getCurrentLayer()->n_kernels++; }
+CNNLayerType CNN::getCurrentLayerType() { return layers[n_layers - 1]->type; }
+CNNLayerType CNN::getPreviousLayerType() { return layers[n_layers - 2]->type; }
+inline ConvolutionLayer* CNN::getConvolutionalData(CNNLayer* layer) { return getConvolutionLayer(layer->data); } 
+inline PoolingLayer* CNN::getPoolingData(CNNLayer* layer) { return getPoolingLayer(layer->data); }
 Kernel* CNN::createNewKernel() { return new Kernel(); }
 Kernel* CNN::createNewKernel(FilterStyle fs) { return new Kernel(fs); }
 Kernel* CNN::createNewKernel(FixedFilterDimensions fd) { return new Kernel(fd); }
@@ -39,11 +36,16 @@ Kernel* CNN::createNewKernel(DynamicFilterDimensions fd, int n) { return new Ker
 Kernel* CNN::createNewKernel(DynamicFilterDimensions fd, int n, FilterStyle fs) { return new Kernel(fd, n, fs); }
 void CNN::appendNewLayer(CNNLayer* layer) { layers.push_back(layer); n_layers++; }
 
+void CNN::appendNewKernel(Kernel* kernel) { 
+    ConvolutionLayer* data = getConvolutionalData(getCurrentLayer());
+    data->kernels.push_back(kernel); 
+    data->n_kernels++; 
+}
 
 CNNLayer* CNN::createNewLayer(CNNLayerType layer_type) {
-    CNNLayer* new_layer = new CNNLayer;
+    CNNLayer* new_layer = new CNNLayer();
 
-    new_layer->layer_type = layer_type;
+    new_layer->type = layer_type;
 
     return new_layer;
 }
@@ -58,14 +60,15 @@ void CNN::printCNN() {
 
     for (int l = 0; l < n_layers; l++) {
         
-        switch (layers[l]->layer_type) {
+        switch (layers[l]->type) {
             case CNN_CONVOLUTION_LAYER: {
-                printf("\t Layer %d: %s - %d Kernels\n", l, CNNLayerStrings[layers[l]->layer_type].c_str(), layers[l]->n_kernels);
+                ConvolutionLayer* cl = getConvolutionLayer(layers[l]->data);
+                printf("\t Layer %d: %s - %d Kernels\n", l, CNNLayerStrings[cl->n_kernels].c_str(), cl->n_kernels);
 
-                for (int k = 0; k < layers[l]->n_kernels; k++) {
-                    Kernel* lk = layers[l]->kernels[k];
+                for (int k = 0; k < cl->n_kernels; k++) {
+                    Kernel* lk = cl->kernels[k];
                     printf("\t\tKernel %d: \n", k);
-                    printf("\t\t\t%s(% dx % d) % s Filter\n", lk->getFilterDimensionsString().c_str(), lk->getFilterWidth(), lk->getFilterHeight(), lk->getFilterStyleString().c_str());
+                    printf("\t\t\t%s(% dx % d) %s Filter\n", lk->getFilterDimensionsString().c_str(), lk->getFilterWidth(), lk->getFilterHeight(), lk->getFilterStyleString().c_str());
                     printf("\t\t\tbias: %lf\n", lk->getBias());
                 }
 
@@ -79,7 +82,7 @@ void CNN::printCNN() {
                 printf("Flattening Layer\n");
                 break;
             }
-            case CNN_NEURAL_NETWORK: {
+            case CNN_FULLY_CONNECTED: {
                 printf("Neural Network Layer\n");
                 break;
             }
