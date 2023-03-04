c41a997767c4bc1051e9581a4426633f1477e2d4
alephpt <alephpt1@gmail.com>
2023-03-04 01:23:11 -0600

    trying to implement threshold reward scalar

--- python/generational/fitness.py
+++ python/generational/fitness.py
@@ -1,5 +1,3 @@
-from functools import total_ordering
-from re import L
 import pygame
 import random
 import math
@@ -7,7 +5,7 @@ import math
 
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 800
-INIT_POP_N = 300
+INIT_POP_N = 20
 FOOD_SIZE = 5
 INIT_SIZE = 5
 
@@ -36,46 +34,77 @@ class Food():
         pygame.draw.rect(screen, self.color, (self.x, self.y, self.r, self.r), 1)
 
 class Individual():
-    evolution_rate = 0.75
+    evolution_rate = 1
     actions = {
         "turn_right": (0, 0.25),
         "turn_left": (0, -0.25),
-        "accelerate": (0.375, 0),
-        "decelerate": (-0.375, 0),
-        "accel_right": (0.375, 0.25),
-        "accel_left": (0.375, -0.25),
-        "decel_right": (-0.375, 0.25),
-        "decel_left": (-0.375, -0.25)
+        "accelerate": (0.5, 0),
+        "decelerate": (-0.5, 0),
+        "accel_right": (0.5, 0.25),
+        "accel_left": (0.5, -0.25),
+        "decel_right": (-0.5, 0.25),
+        "decel_left": (-0.5, -0.25)
     }
     
     def __init__(self):
         self.alive = True
         self.lifetime = 0
+        self.father = None
+        self.mother = None
+        self.partner = None
+        self.children = 0
         self.x = random.random() * SCREEN_WIDTH
         self.y = random.random() * SCREEN_HEIGHT
         self.r = INIT_SIZE
         self.color = (random.randint(178, 255), 0, 0)
         self.direction = math.radians(random.randint(0, 360))  # inherits avg from parents
+        self.max_velocity = 100
+        self.max_acceleration = 10
+        self.max_energy = 2500
         self.velocity = 0
         self.acceleration = 0
-        self.max_energy = 1000                                 # inherits average on next generation
         self.energy = 1000                                     # inherits average on next generation <- should be used for target
-        self.perspective = INIT_SIZE                           #
+        self.perspective = INIT_SIZE                           # inherits from parent
         self.threshold_accel = 0                               # inherits avg_vel on next generation <- should be used for target
         self.threshold_velocity = 0                            # inherits avg_vel on next generation <- should be used for max + 1/2 accel
         self.threshold_energy = 1000                           # inherits (max + avg / 2) on next generation <- should be used for target
         self.threshold_energy_conserv = 0                      # TODO: maybe we create a reward to optimize maintaining energy at converservation level?
         self.threshold_perspective = INIT_SIZE                 # inherits from parents and becomes new default perspective
-        self.avg_accel = 0
-        self.avg_vel = 0
-        self.avg_direction = 0
-        self.avg_perspective = INIT_SIZE
-        self.avg_energy_conservation = 1
-        self.change_in_acceleration = 0
-        self.change_in_direction = 0
-        self.reward = 0
-        self.fitness = 0
-        self.targets_reached = 0
+        self.avg_energy = 0                                    # used for threshold and next gen
+        self.avg_accel = 0                                     # used for threshold and next gen
+        self.avg_vel = 0                                       # used for threshold and next gen
+        self.avg_direction = 0                                 # used for next gen
+        self.avg_perspective = INIT_SIZE                       # used for threshold
+        self.avg_energy_conservation = 1                       # used for threshold
+        self.change_in_acceleration = 0                        # used for energy deduction
+        self.change_in_direction = 0                           # used for energy deduction
+        self.threshold_reward = 0                              # used to scale fitness
+        self.reward = 0                                        # main fitness factor
+        self.fitness = 0                                       # used for genetic evolution
+        self.targets_reached = 0                               # fitness scalar
+    
+    def thresholdReward(self, v, t, m):
+        if v == 0:
+            return 0
+        
+        if t == 0:
+            return 1
+        
+        return (v / t) / (v / m)
+    
+    def determineThresholdReward(self):
+        # acceleration
+        atr = self.thresholdReward(self.acceleration, self.threshold_accel, self.max_acceleration)
+        # velocity
+        vtr = self.thresholdReward(self.velocity, self.threshold_velocity, self.max_velocity)
+        # energy
+        etr = self.thresholdReward(self.energy, self.threshold_energy, self.max_energy)
+        # energy conservation
+        ectr = self.thresholdReward(self.energyConservation(), self.threshold_energy, 1)
+        # perspective
+        ptr = self.thresholdReward(self.perspective, self.threshold_perspective, 1)
+        
+        return (atr + vtr + etr + ectr + ptr)
     
     def energyConservation(self):
         return math.sqrt((self.energy / self.max_energy) ** 2) 
@@ -83,10 +112,10 @@ class Individual():
     def distance(a, b):
         return math.sqrt((b.x - a.x) ** 2 + (b.y - a.y) ** 2)
     
-    #randomly accelerates/decelerates
+    #randomly accelerates/decelerates, and rotates
     def roam(self):
-        self.acceleration = random.uniform(-1, 1)
-        self.direction += random.uniform(-10, 10)
+        self.acceleration += random.uniform(-1.0, 1.0)
+        self.direction += random.uniform(-0.5, 0.5)
         self.energy -= self.perspective / INIT_SIZE
         self.reward -= self.perspective / INIT_SIZE
         self.perspective += self.perspective
@@ -131,12 +160,14 @@ class Individual():
         self.direction += self.change_in_direction
     
     def updateAverages(self):
+        self.avg_energy = (self.avg_energy + self.energy) / 2
         self.avg_vel = (self.avg_vel + self.velocity) / 2
         self.avg_accel = (self.avg_accel + self.acceleration) / 2
         self.avg_direction = (self.avg_direction + self.direction) / 2
         self.avg_perspective = (self.avg_perspective + self.perspective) / 2
         self.avg_energy_conservation = (self.avg_energy_conservation + self.energyConservation()) / 2
-
+        self.threshold_reward += self.determineThresholdReward()
+        
     def updateLocation(self):
         self.velocity += self.acceleration
         
@@ -174,8 +205,6 @@ class Individual():
     def locateTarget(self, target):
         return self.distance(target) < self.perspective
 
-
-
     def navigate(self, targets):
         target_located = False
 
@@ -209,8 +238,6 @@ class Individual():
                     self.targets_reached += 1
                     targets.remove(target)
 
-
-        
         # if no targets are without our perspective
         if not target_located:
             # randomly move and increase perspective
@@ -219,7 +246,7 @@ class Individual():
             self.updateLocation()
 
     def determineFitness(self):
-        return self.reward / self.lifetime * self.avg_energy_conservation
+        return (((self.targets_reached * 10 + self.reward) * self.threshold_reward) / self.lifetime) * self.avg_energy_conservation
 
     def die(self):
         self.fitness = self.determineFitness()
@@ -235,6 +262,7 @@ class Individual():
         print("\t - total reward: \t", self.reward)
         print("\t - total lifetime: \t", self.lifetime)
         print("\t - targets reached: \t", self.targets_reached)
+        print("\t - avg acceleration: \t", self.avg_energy)
         print("\t - avg acceleration: \t", self.avg_accel)
         print("\t - avg velocity: \t", self.avg_vel)
         print("\t - avg direction: \t", self.avg_direction)
@@ -278,7 +306,7 @@ class World():
     
     def maintainFoodSupply(self):
         if len(self.food) < maxFood():
-            if self.food_delay >= (maxFood() / 2):
+            if self.food_delay >= (maxFood() / total_population):
                 self.food.append(Food())
                 self.food_delay = 0
                 return
@@ -318,7 +346,7 @@ def main():
         world.evolve()
         
         pygame.display.update()
-        clock.tick(2)
+        clock.tick(3)
 
 
 if __name__ == "__main__":
