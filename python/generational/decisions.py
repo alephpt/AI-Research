525a1a02a94b5aedd516ff9a50d27b3489ac7145
alephpt <alephpt1@gmail.com>
2023-03-05 09:23:34 -0600

    new data structure

--- python/generational/decisions.py
+++ python/generational/decisions.py
@@ -22,8 +22,8 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 ACTIONS = {
     "forward": (0.2, 0),
     "reverse": (-0.2, 0),
-    "left": (0, -0.0349),
-    "right": (0, 0.0349),
+    "turn_left": (0, -0.0349),
+    "turn_right": (0, 0.0349),
     "forward_left": (0.2, -0.0349),
     "forward_right": (0.2, 0.0349),    
     "reverse_left": (-0.2, -0.0349),
@@ -40,8 +40,10 @@ class Work():
     def __init__(self):
         self.x = random.randint(INIT_EMPLOYER_SIZE, SCREEN_WIDTH - INIT_EMPLOYER_SIZE)
         self.y = random.randint(INIT_EMPLOYER_SIZE, SCREEN_HEIGHT - INIT_EMPLOYER_SIZE)
-        self.s = INIT_EMPLOYER_SIZE
-        self.c = (255, 125, 0)
+        self.size = INIT_EMPLOYER_SIZE
+        self.color = (255, 125, 0)
+        self.energy = -100
+        self.pay = 100
     
     def draw(self):
         pygame.draw.rect(screen, self.c, (self.x, self.y, self.s, self.s), 1)
@@ -50,19 +52,101 @@ class Food():
     def __init__(self):
         self.x = random.randint(INIT_FOOD_SIZE, SCREEN_WIDTH - INIT_FOOD_SIZE)
         self.y = random.randint(INIT_FOOD_SIZE, SCREEN_HEIGHT - INIT_FOOD_SIZE)
-        self.s = INIT_FOOD_SIZE
-        self.c = (0, 183, 69)
+        self.size = INIT_FOOD_SIZE
+        self.color = (0, 183, 69)
+        self.energy = 500
+        self.cost = 10
         
     def draw(self):
-        pygame.draw.rect(screen, self.c, (self.x, self.y, self.s, self.s), 1)
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.size, self.size), 1)
 
 class Individual():
     def __init__(self, id):
+        self.fitness = 0                                       # used for genetic evolution
+        self.lifetime = 0
+        self.total_actions = 0                                 # used for averaging actions
+        self.target = None
+        self.targets_reached = 0                               # fitness scalar
+        self.alive = True
+        self.father = None
+        self.mother = None
+        self.partner = None
+        self.children = 0
+        self.sex = id % 2   # 0 for female, 1 for male
         self.x = random.randint(INIT_INDIVIDUAL_RADIUS, SCREEN_WIDTH - INIT_INDIVIDUAL_RADIUS)
         self.y = random.randint(INIT_INDIVIDUAL_RADIUS, SCREEN_HEIGHT - INIT_INDIVIDUAL_RADIUS)
-        self.s = INIT_INDIVIDUAL_RADIUS
-        self.sex = id % 2   # 0 for female, 1 for male
-        self.c = (255, 125, 125) if self.sex == 0 else (125, 125, 255)
+        self.size = INIT_INDIVIDUAL_RADIUS
+        self.color = (255, 125, 125) if self.sex == 0 else (125, 125, 255)
+        self.perception = INIT_INDIVIDUAL_RADIUS               # inherits from parent
+        self.avg_perception = self.perception                  # inherits from parents and becomes new default perspective
+        ## Energy
+        self.energy = 1250                                     # inherits average on next generation
+        self.avg_energy = self.energy                          # used for threshold and next gen
+        self.threshold_energy = 1000                           # inherits (max + avg) / 2 on next generation
+        self.max_energy = 2500                                 # inherits (max + avg) / 2 on next generation
+        self.energy_score = 0.5                                # used for target_learning table -> passes to nextgen
+        ## Energy Conservation
+        self.avg_energy_conservation = 1                       # used for threshold in next gen
+        self.threshold_energy_conserv = 0                      # used for threshold reward
+        ## Direction
+        self.direction = math.radians(random.randint(0, 360))  # inherits avg from parents
+        self.change_in_direction = 0                           # used for energy deduction
+        self.avg_direction = 0                                 # used for next gen
+        ## Acceleration
+        self.acceleration = 0
+        self.change_in_acceleration = 0                        # used for energy deduction
+        self.avg_acceleration = 0                              # used for threshold and next gen
+        self.threshold_acceleration = 0                        # inherits avg_acceleration on next generation
+        ## Velocity
+        self.velocity = 0
+        self.avg_velocity = 0                                  # inherits avg on nextgen (used as Velocity + Avg / 2)
+        self.target_velocity = 0                               # inherits avg_vel + max / 2 on nextgen (used for threshrold)
+        self.max_velocity = 40                                 # needs to be adjustable
+        ## Satisfaction
+        self.satisfaction = 0
+        self.avg_satisfaction = 0                              # passed to children as new target
+        self.target_satisfaction = 100                         # inherits parents avg on nextgen
+        self.satisfaction_score = 0                            # used for target_learning table
+        ## Money
+        self.money = 0                                         # used to buy food and determine attraction <- mutate this
+        self.money_threshold = 100                             # used to determine score, passed to children
+        self.money_score = 0                                   # used for target_learning
+        ## Rewards
+        self.attractability = 0                                # used to attracting mates - Needs to incorporate energy conservation
+        self.reward = 0                                        # main fitness factor
+        self.threshold_reward = 0                              # used to scale fitness
+        ## Action Tables
+        self.target_learning = {
+            "working": {[self.satisfaction_score, self.energy_score, self.money_score], 1},
+            "eating": {[self.satisfaction_score, self.energy_score, self.money_score], 1},
+            "mating": {[self.satisfaction_score, self.energy_score, self.money_score], 1}
+        }
+        self.action_history = {
+            "forward": 0,
+            "reverse": 0,
+            "turn_left": 0,
+            "turn_right": 0,
+            "forward_left": 0,
+            "forward_right": 0,    
+            "reverse_left": 0,
+            "reverse_right": 0
+        }
+        self.action_bias = {
+            "forward": 1,
+            "reverse": 1,
+            "turn_left": 1,
+            "turn_right": 1,
+            "forward_left": 1,
+            "forward_right": 1,    
+            "reverse_left": 1,
+            "reverse_right": 1
+        }
+    
+    def chooseTarget(self):
+        return
+    
+    def chooseAction(self):
+        return
     
     def draw(self):
         pygame.draw.circle(screen, self.c, (self.x, self.y), self.s)
