55cc9102d9902b4132871e449919e248cb11b4c2
alephpt <alephpt1@gmail.com>
2023-03-09 00:30:26 -0600

    fully connected class

--- python/fully_connected/fully_connected.py
+++ python/fully_connected/fully_connected.py
@@ -53,3 +53,52 @@ class Activation(Layer):
     
     def backward(self, err_out, learning_rate):
         return self.prime(self.input) * err_out
+    
+class Network:
+    def __init__(self):
+        self.layers = []
+        self.loss = None
+        self.prime_loss = None
+    
+    def addLayer(self, layer):
+        self.layers.append(layer)
+        
+    def setLossFn(self, loss, prime):
+        self.loss = loss
+        self.prime_loss = prime
+    
+    def analyze(self, data_in):
+        n_samples = len(data_in)
+        result = []
+        
+        for n in range(n_samples):
+            data_out = data_in[n]
+            
+            for layer in self.layers:
+                data_out = layer.forward(data_out)
+            
+            result.append(data_out)
+            
+        return result
+    
+    def train(self, dataset, classifier, cycles, learning_rate):
+        n_samples = len(dataset)
+        
+        for epoch in range(cycles):
+            err = 0
+            for sample in range(n_samples):
+                data_out = dataset[sample]
+                
+                for layer in self.layers:
+                    data_out = layer.forward(data_out)
+                
+                loss += self.loss(classifier[sample], data_out)
+                err = self.prime_loss(classifier[sample], data_out)
+                
+                for layer in self.layers:
+                    err = layer.backward(err, learning_rate)
+                
+            err /= n_samples
+            
+            if epoch % (n_samples / 20) == 0:
+                print('epoch %d/%d:  err - %f' % (epoch + 1, cycles, err)
