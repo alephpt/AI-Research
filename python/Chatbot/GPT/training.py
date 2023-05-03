a0af940ff774741b873ffce141a14c731da160b7
RC-Off <128416345+RC-Off@users.noreply.github.com>
2023-05-03 00:54:40 -0500

    multi-headed

--- python/Chatbot/GPT/training.py
+++ python/Chatbot/GPT/training.py
@@ -3,6 +3,9 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
 def load_training_data(strings):
     # open all the files in the training_data folder
     files = os.listdir("training_data")
@@ -27,27 +30,71 @@ def construct_training_data(strings, encode):
 
     return training_data, validation_data
 
-class BigramModel(nn.Module):
-    def __init__(self, vocab_size):
+
+class Head(nn.Module):
+    def __init__(self, block_size, head_size, n_embeds=16):
         super().__init__()
-        self.embedding = nn.Embedding(vocab_size, vocab_size)
+        self.key = nn.Linear(n_embeds, head_size, bias=False)
+        self.query = nn.Linear(n_embeds, head_size, bias=False)
+        self.value = nn.Linear(n_embeds, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+    
+    def forward(self, x):
+        Bx, Tx, Cx = x.shape
+        k = self.key(x)
+        q = self.query(x)
+        weight = q @ k.transpose(-2, -1) * (1.0 / (Cx ** 0.5))
+        weight = weight.masked_fill(self.tril[:Tx, :Tx] == 0, float('-inf'))
+        weight = F.softmax(weight, dim=-1)
+        v = self.value(x)
+        out = weight @ v
+        return out
+
+
+class MultiHead(nn.Module):
+    def __init__(self, block_size, head_size, n_embeds=16, n_heads=8):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(block_size, head_size, n_embeds) for _ in range(n_heads)])
+        #self.linear = nn.Linear(n_heads * head_size, n_embeds)
+    
+    def forward(self, x):
+        out = torch.cat([head(x) for head in self.heads], dim=-1)
+        #out = self.linear(out)
+        return out
+
+
+class BigramModel(nn.Module):
+    def __init__(self, block_size, vocab_size, n_embeds=16):
+        super().__init__()
+        self.block_size = block_size
+        self.embedding = nn.Embedding(vocab_size, n_embeds)
+        self.position_embed = nn.Embedding(block_size, n_embeds)
+        self.head = MultiHead(block_size, n_embeds // 8, n_embeds, 8)
+        self.linear_head = nn.Linear(n_embeds, vocab_size)
 
     def forward(self, x, targets=None):
-        logits = self.embedding(x)
+        Bx, Tx = x.shape
+        
+        token_embed = self.embedding(x)
+        position_embed = self.position_embed(torch.arange(Tx, device=device))
+        tx = token_embed + position_embed
+        tx = self.head(tx)
+        logits = self.linear_head(tx)
 
         if targets is None:
             return logits, None
         else :
-            B, T, C = logits.shape
-            logits = logits.view(B*T, C)
-            targets = targets.view(B*T)
+            Bl, Tl, Cl = logits.shape
+            logits = logits.view(Bl*Tl, Cl)
+            targets = targets.view(Bl*Tl)
             loss = F.cross_entropy(logits, targets)
 
             return logits, loss
 
     def generate(self, x, n):
         for _ in range(n):
-            logits, loss = self(x)
+            x_conditional = x[:, -self.block_size:]
+            logits, loss = self(x_conditional)
             logits = logits[:, -1, :]
             probs = F.softmax(logits, dim=-1)
             idx_next = torch.multinomial(probs, num_samples=1)
@@ -56,8 +103,12 @@ class BigramModel(nn.Module):
 
 
 def main():
+    eval_iterations = 250
+    max_iterations = 16725
+    learning_rate = 0.00000725
     batch_size = 32
-    block_size = 8
+    block_size = 16
+    n_embeds = 32
     strings = []
     # TODO: Turn this into WORD Matching instead of CHAR Matching
     # TODO: Implement Sub-Word Schema
@@ -86,8 +137,24 @@ def main():
         ix = torch.randint(len(data) - block_size, (batch_size,))
         x = torch.stack([data[i:i+block_size] for i in ix])
         y = torch.stack([data[i+1:i+block_size+1] for i in ix])
-        return x, y
+        return x.to(device), y.to(device)
 
+    @torch.no_grad()
+    def estimate_loss():
+        out = {}
+        model.eval()
+        
+        for split in ["train", "valid"]:
+            losses = torch.zeros(eval_iterations)
+            for k in range(eval_iterations):
+                xb, yb = get_batch(split)
+                _, loss = model(xb, yb)
+                losses[k] = loss.item()
+            out[split] = losses.mean().item()
+        model.train()
+        
+        return out
+    
     # xb, yb = get_batch("train")
     # print("\nInput Batch:", xb.shape, "\n", xb, "\n")
     # print("Target Batch:", yb.shape, "\n", yb, "\n")
@@ -98,22 +165,27 @@ def main():
     #         target = yb[b, t]
     #         print(f"Context: {context.tolist()} \t Target: {target}")
 
-    m = BigramModel(vocab_size)
+    model = BigramModel(block_size, vocab_size, n_embeds)
+    m = model.to(device)
     # logits, loss = m(xb, yb)
     # print("Logits:", logits.shape)
     # print("Loss:", loss)
 
-    optimizer = torch.optim.Adam(m.parameters(), lr=1e-3)
+    optimizer = torch.optim.Adam(m.parameters(), lr=learning_rate)
 
-    for steps in range(100):
+    for steps in range(max_iterations):
+        if steps % eval_iterations == 0:
+            losses = estimate_loss()
+            print(f"Step: {steps} \t Train Loss: {losses['train']:.4f} \t Valid Loss: {losses['valid']:.4f}")
+            
         xb, yb = get_batch("train")
         logits, loss = m(xb, yb)
-        print("Step:", steps, "\tLoss:", loss.item())
+        
         optimizer.zero_grad(set_to_none=True)
         loss.backward()
         optimizer.step()
 
-        print("Generated:", decode(m.generate(xb, n=10)))
+    print("Generated:", decode(m.generate(x = torch.zeros((1, 1), dtype=torch.long), n=125)[0].tolist()))
 
 
 if __name__ == "__main__":
