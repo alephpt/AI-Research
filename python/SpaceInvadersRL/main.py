d1a7155c6ac1365d0faf1049d68e4d8504c3f28d
alephpt <alephpt@github.com>
2023-06-07 23:36:09 -0500

    modifications and more state. going for 1k

--- python/SpaceInvadersRL/main.py
+++ python/SpaceInvadersRL/main.py
@@ -24,7 +24,7 @@ class QLearningAgent:
     def get_action(self, state):
         if state not in self.q_table:
             self.q_table[state] = [0, 0, 0, 0]
-        
+
         # exploration vs exploitation trade-off (epsilon-greedy)
         if random.random() < self.exploration_rate:
             # explore
@@ -37,63 +37,65 @@ class QLearningAgent:
     def update_q_table(self, state, action, reward, next_state):
         if next_state not in self.q_table:
             self.q_table[next_state] = [0, 0, 0, 0]
-        
+
         if state not in self.q_table:
             self.q_table[state] = [0, 0, 0, 0]
-        
+
         q_value = self.q_table[state][action]
         q_new = reward + self.discount_factor * np.max(self.q_table[next_state])
         self.q_table[state][action] += self.learning_rate * (q_new - q_value)
-    
+
     # update exploration rate
     def update_exploration_rate(self, epoch):
         self.exploration_rate = 0.01 + (1.0 - 0.01) * np.exp(-self.exploration_decay_rate * epoch)
-    
+
 
 def main():
     game = Game()
     testing = False
-    epochs = 100
-    
+    epochs = 1000
+
     # Game Development
-    if testing: 
+    if testing:
         while game.running:
             game.run()
     # Reinforcement Learning
     else:
         print('Reinforcement Learning Started')
         agent = QLearningAgent(len(game.get_states()))
-        
+
         # open q table and assign it to agent
         if os.path.exists('table.qtf'):
             with open('table.qtf', 'r') as f:
                 print('Loading Q Table')
                 agent.q_table = eval(f.read())
                 print('Q Table Loaded')
-            
+
         total_reward = 0
         for epoch in range(epochs):
-            print('Episode: {}/{} Started'.format(epoch, epochs))
+
+            if epoch % (epochs / 10) == 0:
+                print('Episode: {}/{} Started'.format(epoch, epochs))
             match_reward = 0
             while game.running:
-                print('Episode: {}/{} Level: {}, Score: {}, Reward: {}/{}'.format(epoch, epochs, game.level, game.player.score, match_reward, total_reward))
                 game.clock.tick(1200)
                 state = game.get_states()
                 action = agent.get_action(state)
-                reward = game.step(action) // 1000 
+                reward = game.step(action) // 1000
                 game.draw()
-                
+
                 next_state = game.get_states()
                 agent.update_q_table(state, action, reward, next_state)
                 agent.update_exploration_rate(epoch)
                 match_reward += reward
-            total_reward += match_reward
-            print('Episode: {}/{} Ended. Level: {}, Score: {}, Reward: {}/{}'.format(epoch, epochs, game.level, game.player.score, match_reward, total_reward))
+            if epoch % (epochs / 10) == 0:
+                total_reward += match_reward
+                print('Episode: {}/{} Ended. Level: {}, Score: {}, Reward: {}/{}'.format(epoch, epochs, game.level, game.player.score, match_reward, total_reward))
             game.reset()
-            
+
             # save q table
             with open('table.qtf', 'w') as f:
                 f.write(str(agent.q_table))
-    
+
 if __name__ == "__main__":
     main()
